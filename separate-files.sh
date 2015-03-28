7418747b2286feba2359b325a1212c4e513bab24
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2015-03-28 12:24:28 -0400

    adding the first version of the script having problems with the spaces in the paths

---
+++ separate-files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    printf "The correct usage: dupe-finder <srcdir> <destdir>\n"
+    printf "where <scrdir> - the directory to start search from recursively\n"
+    printf "      <destdir> - the directory to MOVE dupes to\n"
+else
+
+srcdir=$1
+destdir=$2
+
+for filename in `find $1 -iname "*.nef"` ; do
+    printf "$filename:"
+
+    pathprefix="${filename%.*}"
+
+    if [ -e "${pathprefix}.jpg" ]; then
+        pathfrom="${pathprefix}.jpg" ;
+    elif [ -e "${pathprefix}.JPG" ]; then
+        pathfrom="${pathprefix}.JPG" ;
+    fi
+
+    if [ -n "$pathfrom" ]; then
+        filefrom="${pathfrom##$srcdir}"
+        pathto="$destdir/$filefrom"
+        printf " moving $pathfrom to $pathto ... "
+        relativedirectory="${pathto%/*.*}"
+        mkdir -p "$relativedirectory" # making sure it exists
+        mv "$pathfrom" "$pathto" && printf "done!"
+    else 
+        printf " no corresponding jpeg found."
+    fi
+
+    printf "\n"
+
+    pathfrom=""
+
+done
+
+fi
