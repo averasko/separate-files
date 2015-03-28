8c92ce77aaad6e86d62ebdf18ca5f5d897734ee6
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2015-03-28 18:08:44 -0400

    clean after yourself

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -2,10 +2,11 @@
 
 # Testing the script
 
-rm -fr tmp
+rm -rf tmp
 mkdir tmp
 cp -r ./pre/* ./tmp/
 ../separate-files.sh ./tmp/srcdir ./tmp/destdir
 diff -r ./post ./tmp || { printf "TEST FAILED!!!\n"; exit 1; }
+rm -rf tmp
 printf "TEST OK\n"
 
