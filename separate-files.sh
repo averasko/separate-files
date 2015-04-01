14027e9d58acf1be59d0f41e9c1af978970b8405
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2015-03-31 21:14:09 -0400

    adding support for the alternative extensions

--- separate-files.sh
+++ separate-files.sh
@@ -1,43 +1,59 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    printf "The correct usage: separate-files <srcdir> <destdir>\n"
-    printf "where <scrdir> - the directory to start search from recursively\n"
+if [ "$#" -lt 2 ] || [ "$#" -gt 4 ] ; then
+    printf "Moves all files with extension <moveext> from <srcdir> to <destdir> for which there is a file with the same name and <baseext> extension recursively. Folder structure is preserved.\n"
+    printf "Usage: separate-files <srcdir> <destdir> [<baseext>] [<moveext>]\n"
+    printf "where <scrdir> - the directory to start search from\n"
     printf "      <destdir> - the directory to MOVE dupes to\n"
+    printf "      <baseext> - the extension of filesto keep in place (NEF by default)\n"
+    printf "      <moveext> - the extension of files to move (JPG by default)\n"
+    printf "ex: separate-files ~/photos ~/photo-dupes NEF JPG <-- moves all jpegs that have corresponding NEFs from photos to photo-dupes for, supposedly, further processing or deleting.\n"
 else
 
 srcdir=$1
 destdir=$2
+baseext=NEF
+moveext=JPG
+
+# setting the extensions if provided
+if [ "$#" -ge 3 ] ; then
+    baseext=$3
+fi
+
+if [ "$#" -ge 4 ] ; then
+    moveext=$4
+fi
+
 
 # reading all the files to process
-saveIFS="$IFS"; IFS=$'\n'; files=( $(find "$1" -iname "*.nef" -type f) ); IFS="$saveIFS"
+saveIFS="$IFS"; IFS=$'\n'; files=( $(find "$1" -iname "*.$baseext" -type f) ); IFS="$saveIFS"
 
 
+# for each file in <srcdir>
 for filename in "${files[@]}" ; do
     printf "$filename:"
 
+    # resetting the variable for each iteration
+    pathfrom=""
+
     pathprefix="${filename%.*}"
 
-    if [ -e "${pathprefix}.JPG" ]; then
-        pathfrom="${pathprefix}.JPG" ;
-    elif [ -e "${pathprefix}.jpg" ]; then
-        pathfrom="${pathprefix}.jpg" ;
+    if [ -e "${pathprefix}.$moveext" ]; then
+        pathfrom="${pathprefix}.$moveext" ;
     fi
 
     if [ -n "$pathfrom" ]; then
         filefrom="${pathfrom##$srcdir}"
         pathto="$destdir/$filefrom"
-        printf " moving $pathfrom to $pathto ... "
+        printf " $pathfrom -> $pathto ..."
         relativedirectory="${pathto%/*.*}"
-        mkdir -p "$relativedirectory" # making sure it exists
-        mv "$pathfrom" "$pathto" && printf "done!"
+        mkdir -p "$relativedirectory" # making sure the target directory exists
+        mv "$pathfrom" "$pathto" && printf " done!"
     else 
-        printf " no corresponding jpeg found."
+        printf " no corresponding $pathfrom file."
     fi
 
     printf "\n"
-
-    pathfrom=""
 
 done
 
--- tests/run-test.sh
+++ tests/run-test.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# Testing the script
+# declaring the list of tests
+declare -a testnames=("test1" "test2")
+declare -a extraparams=("" "DNG png")
 
-rm -rf tmp
-mkdir tmp
-cp -r ./pre/* ./tmp/
-../separate-files.sh ./tmp/srcdir ./tmp/destdir
-diff -r ./post ./tmp || { printf "TEST FAILED!!!\n"; exit 1; }
-rm -rf tmp
-printf "TEST OK\n"
+
+# iterating over test cases
+i=0
+for testname in "${testnames[@]}" ; do
+  printf "Test: ${testname}\n"
+
+  rm -rf tmp
+  mkdir tmp
+  cp -r "./${testname}/pre/" "./tmp/"
+  eval "../separate-files.sh ./tmp/srcdir ./tmp/destdir ${extraparams[$i]} "
+  diff -r "./${testname}/post" "./tmp" || { printf "${testname} FAILED!!!\n"; exit 1; }
+  rm -rf tmp
+
+  printf "$testname OK\n"
+
+  i=$[$i +1]
+done
+
+printf "ALL OK\n"
 
